81da6805b8967eb6f55c61659a9ee6e174541e8b
Ali majidi <a.m.majidi.62@gmail.com>
2026-01-04 18:25:39 +0100

    slow sumFourDivisors

--- c++/testAll/testAll/testAll.cpp
+++ c++/testAll/testAll/testAll.cpp
@@ -22,6 +22,38 @@ bool Function_isPalindrome(int x) {
     return true;
 
 }
+int sumFourDivisors1(vector<int>& nums) {
+    int allsum = 0;
+    for (auto num : nums)
+    {
+        int count = 0;
+        int sum = 0;
+        
+        // Check divisors up to sqrt(num)
+        for (int i = 1; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                if (i * i == num) {
+                    // Perfect square - i is a divisor only once
+                    count++;
+                    sum += i;
+                } else {
+                    // Both i and num/i are divisors
+                    count += 2;
+                    sum += i + num / i;
+                }
+                
+                // Early exit if we have more than 4 divisors
+                if (count > 4) break;
+            }
+        }
+        
+        if (count == 4) allsum += sum;
+    }
+    return allsum;
+
+}
 int main()
 {
     auto ss=Function_isPalindrome(12321);
